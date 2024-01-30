d3df8ce9f598ed4018c80210da48299e2659d72e
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-01-30 20:28:33 -0300

    add number functionality

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+const display = document.querySelector('#display')
+
+function getValue(e) {
+  display.innerHTML += this.innerText
+}
+
+const numbers = document.querySelectorAll('.number')
+
+numbers.forEach((number) => number.addEventListener('click', getValue))
