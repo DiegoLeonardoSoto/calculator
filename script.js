b1f3e06bea732a9156c593166b74124981fb8a15
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-01-30 21:24:41 -0300

    add operators, clear and delete functionalitys

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const display = document.querySelector('#display')
 
-function getValue(e) {
+function addNumber(e) {
   display.innerHTML += this.innerText
 }
 
-const numbers = document.querySelectorAll('.number')
+function addOperator(e) {
+  display.innerHTML += ' ' + this.innerHTML + ' '
+}
 
-numbers.forEach((number) => number.addEventListener('click', getValue))
+function clear(e) {
+  display.innerHTML = ''
+}
+
+function undo(e) {
+  let currentDisplay = display.innerHTML
+
+  if (currentDisplay.length > 0)
+    display.innerHTML = currentDisplay.slice(0, currentDisplay.length - 1)
+}
+
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+const clearBtn = document.querySelector('#clear')
+const arrowBtn = document.querySelector('#arrow')
+
+numbers.forEach((number) => number.addEventListener('click', addNumber))
+operators.forEach((operator) => operator.addEventListener('click', addOperator))
+
+clearBtn.addEventListener('click', clear)
+arrowBtn.addEventListener('click', undo)
