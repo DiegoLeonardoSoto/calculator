74488aee6031c23de33e5269fc81e36efc1d41c8
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-01-31 19:43:27 -0300

    update styles, functionalitys and structure

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const display = document.querySelector('#display')
+const display = document.querySelector('#displayText')
+const placeHolder = document.querySelector('#placeHolder')
+
 let operation = []
 let i = -1
 
@@ -18,12 +20,18 @@ let operatorsFunctions = {
 }
 
 function addNumber(e) {
-  console.log(i)
+  if (isNaN(display.innerHTML[0])) {
+    clear()
+  }
+
+  if (placeHolder.innerText.length > 0) placeHolder.innerText = ''
+
   if (isNaN(operation[i])) {
-    operation.push(this.innerText)
+    operation.push(+this.innerText)
     ++i
   } else {
     operation[i] = operation[i] + this.innerText
+    operation[i] = +operation[i]
   }
 
   display.innerHTML += this.innerText
@@ -32,34 +40,83 @@ function addNumber(e) {
 
 function addOperator(e) {
   ++i
+
+  if (i === 3) {
+    operate(this.innerText)
+  }
+
   operation.push(this.innerText)
   display.innerHTML += ' ' + this.innerText + ' '
   console.log(operation)
 }
 
-function operate(e) {
-  let i = 1
+function operate(from) {
+  console.log(from)
+  let result
+  try {
+    const firstNum = operation.shift(),
+      operator = operation.shift(),
+      secondNum = operation.shift()
 
-  display.innerHTML = operatorsFunctions[operation[i]](
-    +operation[i - 1],
-    +operation[i + 1]
-  )
+    if (isNaN(firstNum) || isNaN(secondNum)) throw new Error()
+
+    /* i = 0 */
+    result = operatorsFunctions[operator](firstNum, secondNum)
+    operation.push(result)
+  } catch (error) {
+    result = 'Syntax Error'
+  } finally {
+    renderResult(result, from)
+  }
+}
+
+function renderResult(result, from) {
+  if (from === '=') {
+    clear()
+    placeHolder.innerText = result
+  } else {
+    i = 1
+    display.innerText = result
+  }
 }
 
 function clear(e) {
   operation = []
-  display.innerHTML = ''
+  i = -1
+  display.innerText = ''
+  placeHolder.innerText = 0
 }
 
 function undo(e) {
-  operation.pop()
   let currentDisplay = display.innerHTML
 
-  if (currentDisplay.length > 0)
-    display.innerHTML =
-      currentDisplay[currentDisplay.length - 1] === ' '
-        ? currentDisplay.slice(0, currentDisplay.length - 3)
-        : currentDisplay.slice(0, currentDisplay.length - 1)
+  if (currentDisplay.length < 0) return ''
+
+  if (currentDisplay[currentDisplay.length - 1] === ' ') {
+    display.innerHTML = currentDisplay.slice(0, currentDisplay.length - 3)
+    operation.pop()
+    --i
+  } else {
+    let lastNum = operation[i].toString()
+    if (lastNum.length > 1) {
+      lastNum = lastNum.slice(0, lastNum.length - 1)
+      operation[i] = +lastNum
+    } else {
+      operation.pop()
+      --i
+    }
+
+    display.innerHTML = currentDisplay.slice(0, currentDisplay.length - 1)
+  }
+}
+
+function changeSign(e) {
+  if (i >= 0 && !isNaN(operation[i])) {
+    operation[i] = operation[i] * -1
+    display.innerText = operation.join(' ')
+  }
+
+  console.log(operation)
 }
 
 const numbers = document.querySelectorAll('.number')
@@ -67,10 +124,12 @@ const operators = document.querySelectorAll('.operator')
 const clearBtn = document.querySelector('#clear')
 const arrowBtn = document.querySelector('#arrow')
 const equalBtn = document.querySelector('#equal')
+const changeSignBtn = document.querySelector('#changeSign')
 
 numbers.forEach((number) => number.addEventListener('click', addNumber))
 operators.forEach((operator) => operator.addEventListener('click', addOperator))
 
 clearBtn.addEventListener('click', clear)
 arrowBtn.addEventListener('click', undo)
-equalBtn.addEventListener('click', operate)
+changeSignBtn.addEventListener('click', changeSign)
+equalBtn.addEventListener('click', (e) => operate('='))
